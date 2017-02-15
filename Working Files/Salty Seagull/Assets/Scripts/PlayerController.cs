fd8625aa708161fd5f84a3b3f026742536bc4e5e
jb-aero <jb_aero@zeoldcraft.com>
2017-02-15 17:54:25 -0500

    Added sea, world boarder, and vertical rotation.

--- Working Files/Salty Seagull/Assets/Scripts/PlayerController.cs
+++ Working Files/Salty Seagull/Assets/Scripts/PlayerController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, yMax, zMin, zMax;
+}
+
 public class PlayerController : MonoBehaviour {
 
-	public float moveSpeed, rotationLR = 2.0f, rotationUD = 2.0f;
-	private float yaw = 0.0f, pitch = 0.0f;
+	public float moveSpeed = 10.0f, rotationLR = 2.0f, rotationUD = 2.0f;
 	public Text scoreText, winText;
+
+	public Boundary boundary;
+	public Transform sea;
 
 	private Rigidbody rb;
 	private int count;
+	private float yaw = 0.0f, pitch = 0.0f;
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();
@@ -26,12 +35,19 @@ public class PlayerController : MonoBehaviour {
 		float moveVertical = Input.GetAxis("Vertical");
 
 		yaw += rotationLR * Input.GetAxis("Horizontal");
-		pitch -= rotationUD * Input.GetAxis("Mouse Y");
+		pitch -= rotationUD * Input.GetAxis("Jump") - rotationUD * Input.GetAxis("Fire2");
 
 		Vector3 movement = new Vector3 (0.0f, 0.0f, moveVertical);
 
 		rb.AddRelativeForce (movement * moveSpeed);
 		transform.eulerAngles = new Vector3 (pitch, yaw, 0);
+
+		rb.position = new Vector3
+		(
+			Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+			Mathf.Clamp(rb.position.y, sea.position.y, boundary.yMax),
+			Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+		);
 	}
 
 	void OnTriggerEnter(Collider other)
