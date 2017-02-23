dccfc4ca5c1594076b2c0b51fbce924e19e294fd
Robert Bradshaw <brobert3@umbc.edu>
2017-02-23 04:59:36 -0500

    Added a way to drop items
    Press RB on controller and seagull stops holding the item it has!
    Doesn't include physics yet, unfortunately. Also renamed "JimVersion"
    "SinglePlayer" Which apparently makes me recommit all of the files
    inside.

--- Working Files/Salty Seagull/Assets/Scripts/PlayerController.cs
+++ Working Files/Salty Seagull/Assets/Scripts/PlayerController.cs
@@ -21,123 +21,131 @@ public enum Steering
 	KEYS, MOUSE, JOYSTICK
 }
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	public Text scoreText, winText, warnText, timer;
-	public int requiredScore = 10;
+    public Text scoreText, winText, warnText, timer;
+    public int requiredScore = 10;
 
-	public Boundary boundary;
-	public SeagullLimits limits;
-	public Steering steering;
+    public Boundary boundary;
+    public SeagullLimits limits;
+    public Steering steering;
 
-	public Transform sea;
+    public Transform sea;
 
-	private Rigidbody rb;
-	private int count;
-	private float yaw, pitch;
-	private bool holding = false;
-
-	private float moveVertical, turnUD, turnLR;
+    private Rigidbody rb;
+    private int count;
+    private float yaw, pitch;
+    private bool holding = false;
+    private float moveVertical, turnUD, turnLR;
 
     Transform heldObject;
 
-	void Start ()
-	{
-		rb = GetComponent<Rigidbody>();
-		count = 0;
-		yaw = transform.rotation.eulerAngles.y;
-		pitch = transform.rotation.eulerAngles.x;
-		updateScore ();
-		winText.text = "";
-		rb.freezeRotation = true;
-	}
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        yaw = transform.rotation.eulerAngles.y;
+        pitch = transform.rotation.eulerAngles.x;
+        updateScore();
+        winText.text = "";
+        rb.freezeRotation = true;
+    }
 
-	// Called for physics
-	void FixedUpdate()
-	{
-		moveVertical = Input.GetAxis("Vertical");
+    // Called for physics
+    void FixedUpdate()
+    {
+        moveVertical = Input.GetAxis("Vertical");
 
-		switch (steering)
-		{
-		case Steering.KEYS:
-			turnUD = (Input.GetAxis ("Fire3") - Input.GetAxis ("Fire2"));
-			turnLR = Input.GetAxis ("Horizontal");
-			break;
-		case Steering.MOUSE:
-			turnUD = Input.GetAxis ("Mouse Y");
-			turnLR = Input.GetAxis ("Mouse X");
-			limits.tilt = 0;
-			break;
-		case Steering.JOYSTICK:
-			turnUD = -Input.GetAxis ("Mouse Y");
-			turnLR = Input.GetAxis ("Mouse X");
-			break;
-		}
+        switch (steering)
+        {
+            case Steering.KEYS:
+                turnUD = (Input.GetAxis("Fire3") - Input.GetAxis("Fire2"));
+                turnLR = Input.GetAxis("Horizontal");
+                break;
+            case Steering.MOUSE:
+                turnUD = Input.GetAxis("Mouse Y");
+                turnLR = Input.GetAxis("Mouse X");
+                limits.tilt = 0;
+                break;
+            case Steering.JOYSTICK:
+                turnUD = -Input.GetAxis("Mouse Y");
+                turnLR = Input.GetAxis("Mouse X");
+                //press bumper to drop the object
+                if (Input.GetKeyDown(("joystick 1 button " + 5.ToString())))
+                {
+                    holding = false;
+                }
+                break;
+        }
 
-		yaw += limits.rotationLR * turnLR;
-		pitch -= limits.rotationUD * turnUD;
+        yaw += limits.rotationLR * turnLR;
+        pitch -= limits.rotationUD * turnUD;
 
-		pitch = Mathf.Clamp (pitch, -limits.upAngle, limits.downAngle);
+        pitch = Mathf.Clamp(pitch, -limits.upAngle, limits.downAngle);
 
-		Vector3 movement = -transform.InverseTransformDirection (rb.velocity) * limits.antiDrift;
-		if (moveVertical > 0 || (moveVertical < 0 && movement.z < 0))
-		{
-			movement.z = moveVertical * limits.accelSpeed;
-		}
-		else
-		{
-			movement.z = movement.z / limits.antiDrift * limits.glideDecel;
-		}
+        Vector3 movement = -transform.InverseTransformDirection(rb.velocity) * limits.antiDrift;
+        if (moveVertical > 0 || (moveVertical < 0 && movement.z < 0))
+        {
+            movement.z = moveVertical * limits.accelSpeed;
+        }
+        else
+        {
+            movement.z = movement.z / limits.antiDrift * limits.glideDecel;
+        }
 
-		rb.AddRelativeForce (movement);
+        rb.AddRelativeForce(movement);
 
-		// rb.MoveRotation (rb.rotation * Quaternion.Euler( new Vector3 (pitch, yaw, 0)));
-		rb.rotation = Quaternion.Euler (pitch, yaw, turnLR * -limits.tilt);
-		// rb.rotation = Quaternion.Euler (pitch, yaw, 0);
+        // rb.MoveRotation (rb.rotation * Quaternion.Euler( new Vector3 (pitch, yaw, 0)));
+        rb.rotation = Quaternion.Euler(pitch, yaw, turnLR * -limits.tilt);
+        // rb.rotation = Quaternion.Euler (pitch, yaw, 0);
 
-		rb.position = new Vector3
-		(
-			Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
-			Mathf.Clamp(rb.position.y, sea.position.y, boundary.yMax),
-			Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
-		);
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            Mathf.Clamp(rb.position.y, sea.position.y, boundary.yMax),
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
 
         //move the held object with you
         if(holding)
         {
             heldObject.position = transform.FindChild("MountPoint").transform.position;
         }
-	}
 
-	void OnTriggerEnter(Collider other)
-	{
-		if (other.gameObject.CompareTag("pickup"))
-		{
-			if (holding)
-			{
-				// Display warning message
-			}
-			else
-			{
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("pickup"))
+        {
+            if(holding)
+            {
+                // Display warning message
+            }
+            else
+            {
                 //other.gameObject.SetActive(false);
                 heldObject = other.transform;
-				holding = true;
-			}
-		}
-		else if (other.gameObject.CompareTag("nest"))
-		{
-			holding = false;
-			count++;
-			updateScore ();
-		}
-	}
+                holding = true;
+            }
+        }
+        if(other.CompareTag("nest"))
+        {
+            holding = false;
+            count++;
+            updateScore();
+        }
+    }
 
-	void updateScore()
-	{
-		scoreText.text = "Score: " + count.ToString ();
-		if (count >= requiredScore)
-		{
-			winText.text = "YOU WIN!!";
-		}
-	}
+    void updateScore()
+    {
+        scoreText.text = "Score: " + count.ToString();
+        if (count >= requiredScore)
+        {
+            winText.text = "YOU WIN!!";
+        }
+    }
 }
