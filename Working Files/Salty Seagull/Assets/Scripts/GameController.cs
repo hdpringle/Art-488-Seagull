c9c418898e116a4a15dcd72f62071b1c78265a9b
jb-aero <jb_aero@zeoldcraft.com>
2017-02-27 18:40:42 -0500

    Started on timed game control.

---
+++ Working Files/Salty Seagull/Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public float timeLimitSeconds;
+	public Text timer;
+
+	private float currentTime;
+	private int minutes, seconds;
+
+	// Use this for initialization
+	void Start () {
+		currentTime = timeLimitSeconds;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		while (currentTime > 0) {
+			currentTime -= Time.deltaTime;
+			minutes = ((int)currentTime) / 60;
+			seconds = ((int)currentTime) % 60;
+
+			timer.text = minutes + ":" + seconds;
+		}
+	}
+}
