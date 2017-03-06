08a15f53c65323a091df4fcf9dd37dd8d373f7a9
Robert Bradshaw <brobert3@umbc.edu>
2017-03-06 18:10:12 -0500

    Added mount points on pick up objects
    The seagull picks objects up by speciffic mounting point.

--- Working Files/Salty Seagull/Assets/Scripts/PlayerController.cs
+++ Working Files/Salty Seagull/Assets/Scripts/PlayerController.cs
@@ -132,8 +132,15 @@ public class PlayerController : MonoBehaviour
         //move the held object with you
         if(holding)
         {
-            heldObject.position = transform.FindChild("MountPoint").transform.position;
-        }
+			/*8
+			//should set the held objects mount point's position equal to the seagull's mount point position
+            heldObject.FindChild("MountPoint").transform.position = transform.FindChild("MountPoint").transform.position;
+
+			//should update the object to move with it's mount point
+			heldObject.position = heldObject.Find("MountPoint").transform.position - heldObject.GetComponent<Pickups>().offset;
+			*/
+			heldObject.position = transform.FindChild("MountPoint").transform.position - (heldObject.FindChild("MountPoint").transform.position - heldObject.position);
+		}
 
 
     }
@@ -156,7 +163,7 @@ public class PlayerController : MonoBehaviour
         if(other.CompareTag("nest"))
         {
             holding = false;
-            count++;
+            //count++;
             updateScore();
         }
     }
