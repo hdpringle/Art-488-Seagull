1c270e49492ccdb9d8cb181d5681b680a639498c
Robert Bradshaw <arejaybee95@gmail.com>
2017-04-13 07:50:17 -0400

    Player Select Menu (WIP)
    Added a scene for player selects, and it lets you swipe through various
    Seagull options. Updates to come. Put all of the UI related materials
    into their own folder for orginaziation's sake. Seagull skins are in
    "Resources/Materials"

---
+++ Working Files/Salty Seagull/Assets/Scripts/PlayerSelecter.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSelecter : MonoBehaviour {
+	const int NUMBER_OF_SEAGULLS = 4;//can update when more seagull skins come
+
+	public int playerNumber;
+	
+	public int currentSeagull;//the seagull selected
+
+	public bool chosen;//has the player chosen a seagull?
+
+	private class InputValues
+	{
+		public float turnLR;
+		public float select;
+	}
+	private InputValues input;
+	// Use this for initialization
+	void Start ()
+	{
+		input = new InputValues();
+		chosen = false;
+		//constantly calls an update function, every 0.25 seconds. Lets us get delays on the controller input.
+		InvokeRepeating("BestUpdate", 0f, 0.25f);
+	}
+
+	void Update()
+	{
+		//set the pic to be the right seagull
+		transform.FindChild("Image").GetComponentInChildren<Image>().material = Resources.Load("Materials/Seagull"+currentSeagull, typeof(Material)) as Material;
+
+		//hit a, you chose
+		if(input.select > 0)
+		{
+			chosen = true;
+		}
+		//hit b, you unchose
+		else if(input.select < 0)
+		{
+			chosen = false;
+		}
+
+		//let the player see their choice
+		if(chosen)
+		{
+			transform.FindChild("Text").GetComponent<Text>().text = "DONE!";
+		}
+		else
+		{
+				transform.FindChild("Text").GetComponent<Text>().text = "Choosing";
+		}
+
+		//get the controls
+		GetControls();
+	}
+
+	// Update is called once per frame
+	public void BestUpdate()
+	{
+		//dont move if you chose
+		if (!chosen)
+		{
+			//swipe right
+			if (input.turnLR > 0)
+			{
+				currentSeagull++;
+				if (currentSeagull > NUMBER_OF_SEAGULLS)
+				{
+					currentSeagull -= NUMBER_OF_SEAGULLS;
+				}
+			}
+			//swipe left
+			else if (input.turnLR < 0)
+			{
+				currentSeagull--;
+				if (currentSeagull <= 0)
+				{
+					currentSeagull += NUMBER_OF_SEAGULLS;
+				}
+			}
+		}
+	}
+	protected virtual void GetControls()
+	{
+		switch (playerNumber)
+		{
+			case 1:
+				input.turnLR = Input.GetAxis("Horizontal");
+				input.select = Input.GetAxis("Submit");
+				break;
+
+			default:
+				input.turnLR = Input.GetAxis("P" + playerNumber + " Horizontal");
+				break;
+		}
+	}
+}
